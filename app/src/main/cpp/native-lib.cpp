cc706b4165a9a78c51a44ebcfa5fe0010d47bb03
reykou <13825713@qq.com>
2020-07-13 18:06:50 +0800

    2

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,6 +5,6 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_tsits_test0000_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++111";
+    std::string hello = "Hello from C++222222";
     return env->NewStringUTF(hello.c_str());
 }
